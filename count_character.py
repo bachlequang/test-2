cd8c23dcacc37768a7088c7fec54800cedd1687f
bachlequang <bachlequang@vccorp.vn>
2011-11-10 15:55:43 +0700

    Update count_character.py

--- count_character.py
+++ count_character.py
@@ -1,19 +1,22 @@
 
-
+import string
 txt=open("count.txt", 'r')
 txtf=txt.read()
 
 #txtf='Python is a cool language but OCaml is even cooler since it is purely functional'
 count ={}
 a={}
+#line=0
+
 
 for words in txtf.split():
+    words = string.lower(words)
     #for line in txtf:
     #if txtf.count(words) == 1:
     #if words  in txtf:
         #count[words]=txtf.count(words)
-        count[words]=count.get(words,0)+1
-#   {'a': 3, 'c': 3, 'e': 2, 'd': 1, 'g': 1, 'f': 1, ... }
+    count[words]=count.get(words,0)+1
+#   {'is': 3, 'Python': 1, 'a': 1... }
                 
         #if words in line:
            # a[words]=int(line)
@@ -34,7 +37,5 @@ for words in txtf.split():
 sortedchar_out_put = sorted(count.items())
 for charoutput in sortedchar_out_put:
      print "%d %s" % (charoutput[1], charoutput[0])
-
-
 
 txt.close()
